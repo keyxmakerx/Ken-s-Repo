40503c43c951915656f7c7fbe4dee1110c6e1e4c
keyxmakerx <keyxmakerx@gmail.com>
2017-02-05 17:40:16 -0600

    Update search.sh
    Edited per Serg's (@Askubuntu.com) advise.

--- search.sh
+++ search.sh
@@ -10,7 +10,7 @@ read -p "Please Enter the Beggining Time. Exp. Aug 1 00:00:01 " date_2;
 while :
  do
     read -p "Is this Date correct? @date_1" choice
-    case ${choice,,} in
+    case ${choice} in
         y|ye|yes) break;;
         n|no) echo "Try again"; exec $0;;
     esac
@@ -18,11 +18,11 @@ done
 while :
  do
     read -p "Is this Date correct? @date_2" choice
-    case ${choice,,} in
+    case ${choice} in
         y|ye|yes) break;;
         n|no) echo "Try again"; exec $0;;
     esac
 done
 
 echo $date_1 , $date_2
-find /srv/log/mail -mtime $(date +%s -d"$date_1") -mtime $(date +%s -d"$date_2")
+find /srv/log/mail - $(date +%s -d"$date_1") - $(date +%s -d"$date_2")
