f89b3c818b18ae6691e5727afa9a3db39bcfbf65
keyxmakerx <keyxmakerx@gmail.com>
2017-02-25 14:59:56 -0600

    Update search.sh
    Complete Rewrite to work with .py script

--- search.sh
+++ search.sh
@@ -2,27 +2,18 @@
 
 date_1=''
 date_2=''
+email=''
 
 read -p "Please Enter the Beggining Time. Exp. Aug 1 00:00:01 " date_1;
 
-read -p "Please Enter the Beggining Time. Exp. Aug 1 00:00:01 " date_2;
+read -p "Please Enter the End Time. Exp. Aug 2 00:00:01 " date_2;
 
-while :
- do
-    read -p "Is this Date correct? $date_1" choice
-    case ${choice} in
-        y|ye|yes) break;;
-        n|no) echo "Try again"; exec $0;;
-    esac
-done
-while :
- do
-    read -p "Is this Date correct? $date_2" choice
-    case ${choice} in
-        y|ye|yes) break;;
-        n|no) echo "Try again"; exec $0;;
-    esac
-done
+read -p "Please Enter the Email in question. Exp. tech@example.com " email;
 
-echo $date_1 , $date_2
-find /srv/log/mail -mtime $(date +%s -d"$date_1") -mtime $(date +%s -d"$date_2")
+sleep 2
+
+echo $date_1 , $date_2 , $email
+
+
+
+./read_log_range.py $date_2  $date_1 /var/log/syslog  | grep $email 
